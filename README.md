c222917ab515946eda63eb286d84222c4db8f1bf
phugh <peter@phugh.es>
2017-08-27 14:48:04 +0100

    1.0.0-rc.1 - Add options - Improve documentation - Abstract shared function to external module (lex-helpers) - Switch to Google code style - Swtich from depreciated 'root' to 'global'

--- README.md
+++ README.md
@@ -1,43 +1,96 @@
 # prospectimo
 
-Get the temporal orientation of a string.
+Get the temporal orientation (time perspective) of a string.
 
 ## Usage
-```Javascript
+```javascript
 const prospectimo = require('prospectimo');
 const opts = {
-  'return': 'lex',         // 'orientation' returns a string, 'lex' (default) returns object of lexical values
-  'encoding': 'binary',    // 'binary' (default), or 'frequency' - type of word encoding to use.
-  'threshold': -0.98       // a lexical weight threshold between 1.16 (include nothing), and -0.98 (include everything, default)
-}
-const str = "A big long string of text...";
+  'encoding': 'binary',
+  'max': Number.POSITIVE_INFINITY,
+  'min': Number.NEGATIVE_INFINITY,
+  'nGrams': true,
+  'output': 'orientation',
+  'places': 9,
+  'sortBy': 'freq',
+  'wcGrams': false,
+};
+const str = 'A big long string of text...';
 const orientation = prospectimo(str, opts);
 console.log(orientation);
 ```
 
-## Options
+## The Options Object
 
-### 'return'
-
-Valid options: 'lex' (default), or 'orientation'.
-
-'lex' returns an object with 'PAST', 'PRESENT' and 'FUTURE' keys, each containing a lexical value for that orientation.
-
-'orientation' returns a string stating either 'Past', 'Present', 'Future', or 'Unknown'.
+The options object is optional and provides a number of controls to allow you to tailor the output to your needs. However, for general use it is recommended that all options are left to their defaults.
 
 ### 'encoding'
 
-Valid options: 'binary' (default), or 'frequency'.
+**String - valid options: 'freq' (default), 'binary'**
 
-'binary' calculates the lexical value as simply a sum of weights, i.e. weight[1] + weight[2] + etc...
+N.B - You probably don't want to change this, ever.
 
-'frequency' calculates the lexical value as (word frequency / total wordcount) * word weight
+Controls how the lexical value is calculated.
 
-Unless you have a specific need for frequency encoding, we recommend you use binary only.
+Binary is simply the addition of lexical weights, i.e. word1 + word2 + word3.
 
-### 'threshold'
+Frequency encoding takes the overall wordcount and word frequency into account, i.e. (word frequency / word count) * weight.
 
-The lexicon contains weight values that are very small. You can exclude them using the threshold option.
+### 'output'
+
+**String - valid options: 'orientation' (default), 'matches', 'lex', or 'full'**
+
+'orientation' (default) returns the predicted temporal orientation as a string, i.e. "Past", "Present", "Future", or "Unknown".
+
+'lex' returns an object with the lexical value for past, present and future orientations.
+
+'matches' returns an array of matched words along with the number of times each word appears, its weight, and its final lexical value. See the output example below for an example.
+
+'full' returns an object containing the lexical value and the matches array.
+
+### 'nGrams'
+
+**Boolean - valid options: true (default) or false**
+
+n-Grams are contiguous pieces of text, bi-grams being chunks of 2, tri-grams being chunks of 3, etc.
+
+Use the nGrams option to include (true) or exclude (false) n-grams. For accuracy it is recommended that n-grams are included, however including n-grams for very long strings can detrement performance.
+
+### 'wcGrams'
+
+**Boolean - valid options: true or false (default)**
+
+When set to true, the output from the nGrams option will be added to the word count.
+
+For accuracy it is recommended that this is set to false.
+
+### 'sortBy'
+
+**String - valid options: 'freq' (default)', 'weight', or 'lex'**
+
+If 'output' = 'matches', this option can be used to control how the outputted array is sorted.
+
+'lex' sorts by final lexical value, i.e. (word frequency * word count) / word weight.
+
+'weight' sorts the array by the matched words initial weight.
+
+'freq' (default) sorts by word frequency, i.e. the most used words appear first.
+
+### 'places'
+
+**Number**
+
+Number of decimal places to limit outputted values to.
+
+The default is 9 decimal places.
+
+### 'max' and 'min'
+
+**Float**
+
+Exclude words that have weights above the max threshold or below the min threshold.
+
+By default these are set to infinity, ensuring that no words from the lexicon are excluded.
 
 The smallest value in the lexicon is -0.9772179. Therefore a threshold of -0.98 will include all words in the lexicon.
 
@@ -46,14 +99,14 @@ The largest value in the lexicon is 1.15807005. Therefore a threshold of 1.16 wi
 ## Acknowledgements
 
 ### References
-Park, G., Schwartz, H.A., Sap, M., Kern, M.L., Weingarten, E., Eichstaedt, J.C., Berger, J., Stillwell, D.J., Kosinski, M., Ungar, L.H. & Seligman, M.E. (2015). Living in the Past, Present, and Future: Measuring Temporal Orientation with Language. Journal of personality.
+[Schwartz, H. A., Park, G., Sap, M., Weingarten, E., Eichstaedt, J., Kern, M., Stillwell, D., Kosinski, M., Berger, J., Seligman, M., & Ungar, L. (2015). Extracting Human Temporal Orientation from Facebook Language. NAACL-2015: Conference of the North American Chapter of the Association for Computational Linguistics.](http://www.seas.upenn.edu/~hansens/tempor-naacl15-cr.pdf).
+
+[Park, G., Schwartz, H.A., Sap, M., Kern, M.L., Weingarten, E., Eichstaedt, J.C., Berger, J., Stillwell, D.J., Kosinski, M., Ungar, L.H. & Seligman, M.E. (2015). Living in the Past, Present, and Future: Measuring Temporal Orientation with Language. Journal of personality.](http://wwbp.org/papers/Park_et_al-2016-Journal_of_Personality.pdf).
 
 ### Lexicon
-Using the prospection lexicon data from [WWBP](http://www.wwbp.org/lexica.html)
+Using the prospection lexicon data from [WWBP](http://www.wwbp.org/lexica.html) under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/).
 
-Used under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/)
+## Licence
+(C) 2017 [P. Hughes](https://www.phugh.es).
 
-# Licence
-(C) 2017 [P. Hughes](www.phugh.es)
-
-[Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/)
+[Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/).
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * prospectimo
- * v0.4.0
+ * v1.0.0-rc.1
  *
  * Analyse the temporal orientation of a string.
  *
@@ -8,9 +8,15 @@
  * https://github.com/phugh/prospectimo
  *
  * Based on this paper:
- * Park, G., Schwartz, H.A., Sap, M., Kern, M.L., Weingarten, E., Eichstaedt, J.C., Berger, J., Stillwell, D.J., Kosinski, M., Ungar, L.H. & Seligman, M.E. (2015). Living in the Past, Present, and Future: Measuring Temporal Orientation with Language. Journal of personality.
+ * Park, G., Schwartz, H.A., Sap, M., Kern, M.L., Weingarten, E., Eichstaedt,
+ * J.C., Berger, J., Stillwell, D.J., Kosinski, M., Ungar, L.H. &
+ * Seligman, M.E. (2015).
+ * Living in the Past, Present, and Future: Measuring Temporal Orientation with
+ * Language. Journal of personality.
+ *
  * Using the prospection lexicon data from http://www.wwbp.org/lexica.html
- * Used under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported licence
+ * Used under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported licence
  *
  * (C) 2017 P. Hughes
  * Licence : Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
@@ -19,216 +25,185 @@
  * Usage example:
  * const prospectimo = require('prospectimo');
  * const opts = {
- *  'return': 'lex',         // 'orientation' returns a string, 'lex' (default) returns object of lexical values
- *  'encoding': 'binary',    // 'binary' (default), or 'frequency' - type of word encoding to use.
- *  'threshold': -0.38      //
- * }
- * const str = "A big long string of text...";
+ *  'encoding': 'binary',
+ *  'max': Number.POSITIVE_INFINITY,
+ *  'min': Number.NEGATIVE_INFINITY,
+ *  'nGrams': true,
+ *  'output': 'orientation',
+ *  'places': 9,
+ *  'sortBy': 'freq',
+ *  'wcGrams': false,
+ * };
+ * const str = 'A big long string of text...';
  * const orientation = prospectimo(str, opts);
  * console.log(orientation)
  *
+ * See README.md for help.
+ *
  * @param {string} str  input string
  * @param {Object} opts options
- * @return {(Object||string)} temporal orientation or lexical value based on opts
+ * @return {(Object|string)}
  */
 
 'use strict'
-;(function () {
-  const root = this
-  const previous = root.prospectimo
+;(function() {
+  const global = this;
+  const previous = global.prospectimo;
 
-  let lexicon = root.lexicon
-  let simplengrams = root.simplengrams
-  let tokenizer = root.tokenizer
+  let lexHelpers = global.lexHelpers;
+  let lexicon = global.lexicon;
+  let simplengrams = global.simplengrams;
+  let tokenizer = global.tokenizer;
 
   if (typeof lexicon === 'undefined') {
     if (typeof require !== 'undefined') {
-      lexicon = require('./data/lexicon.json')
-      simplengrams = require('simplengrams')
-      tokenizer = require('happynodetokenizer')
-    } else throw new Error('prospectimo requires happynodetokenizer and simplengrams, and ./data/lexicon.json')
+      lexHelpers = require('lex-helpers');
+      lexicon = require('./data/lexicon.json');
+      simplengrams = require('simplengrams');
+      tokenizer = require('happynodetokenizer');
+    } else throw new Error('prospectimo required modules not found!');
   }
 
-    /**
-   * Get the indexes of duplicate elements in an array
-   * @function indexesOf
-   * @param  {Array} arr input array
-   * @param  {string} el element to test against
-   * @return {Array} array of indexes
-   */
-  const indexesOf = (arr, el) => {
-    const idxs = []
-    let i = arr.length
-    while (i--) {
-      if (arr[i] === el) {
-        idxs.unshift(i)
-      }
-    }
-    return idxs
-  }
+  const arr2string = lexHelpers.arr2string;
+  const calcLex = lexHelpers.calcLex;
+  const getMatches = lexHelpers.getMatches;
+  const prepareMatches = lexHelpers.prepareMatches;
 
-  /**
-   * Combines multidimensional array elements into strings
-   * @function arr2string
-   * @param  {Array} arr input array
-   * @return {Array} output array
-   */
-  const arr2string = arr => {
-    let i = 0
-    const len = arr.length
-    const result = []
-    for (i; i < len; i++) {
-      result.push(arr[i].join(' '))
-    }
-    return result
-  }
+  const doMatches = (matches, sortBy, wordcount, places, encoding) => {
+    const match = {};
+    match.PAST = prepareMatches(matches.PAST, sortBy, wordcount, places,
+        encoding);
+    match.PRESENT = prepareMatches(matches.PRESENT, sortBy, wordcount, places,
+        encoding);
+    match.FUTURE = prepareMatches(matches.FUTURE, sortBy, wordcount, places,
+        encoding);
+    return match;
+  };
 
-  /**
-  * Loop through lexicon and match against array
-  * @function getMatches
-  * @param  {Array} arr token array
-  * @param  {number} threshold  min. weight threshold
-  * @return {Object} object of matches
-  */
-  const getMatches = (arr, threshold) => {
-    // error prevention
-    if (arr == null) return null
-    if (threshold == null) threshold = -999
-    if (typeof threshold !== 'number') threshold = Number(threshold)
-    // loop through categories in lexicon
-    const matches = {}
-    let category
-    for (category in lexicon) {
-      if (!lexicon.hasOwnProperty(category)) continue
-      let match = []
-      let word
-      let data = lexicon[category]
-      // loop through words in category
-      for (word in data) {
-        if (!data.hasOwnProperty(word)) continue
-        let weight = data[word]
-        // if word from input matches word from lexicon ...
-        if (arr.indexOf(word) > -1 && weight > threshold) {
-          let count = indexesOf(arr, word).length // number of times the word appears in the input text
-          match.push([word, count, weight])
-        }
-      }
-      matches[category] = match
-    }
-    return matches
-  }
-
-  /**
-  * Calculate the total lexical value of matches
-  * @function calcLex
-  * @param {Object} obj matches object
-  * @param {number} wc wordcount
-  * @param {number} int intercept value
-  * @param {string} enc encoding
-  * @return {number} lexical value
-  */
-  const calcLex = (obj, wc, int, enc) => {
-    if (obj == null) return null
-    let lex = 0
-    let word
-    for (word in obj) {
-      if (!obj.hasOwnProperty(word)) continue
-      if (enc === 'binary' || enc == null || wc == null) {
-        // weight + weight + weight etc
-        lex += Number(obj[word][2])
-      } else {
-        // (frequency / wordcount) * weight
-        lex += (Number(obj[word][1]) / Number(wc)) * Number(obj[word][2])
-      }
-    }
-    if (int != null) lex += Number(int)
-    return lex
-  }
+  const doValues = (matches, places, encoding, wordcount) => {
+    const values = {};
+    values.PAST = calcLex(matches.PAST, (-0.649406376419), places, encoding,
+        wordcount);
+    values.PRESENT = calcLex(matches.PRESENT, 0.236749577324, places, encoding,
+        wordcount);
+    values.FUTURE = calcLex(matches.FUTURE, (-0.570547567181), places, encoding,
+        wordcount);
+    return values;
+  };
 
   /**
   * Converts the lexical values object to an orientation string
   * @function getOrientation
-  * @param  {Object} obj lexical values object
-  * @return {string} 'Past', 'Present', or 'Future'
+  * @param  {Object} obj    lexical values object
+  * @return {string} 'Past', 'Present', 'Future', or 'Unknown'
   */
-  const getOrientation = obj => {
-    const a = [obj.PAST, obj.PRESENT, obj.FUTURE]
-    const indexOfMaxValue = a.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)
-
-    let orientation = `No temporal orientation detected.`
+  const getOrientation = (obj) => {
+    const a = [obj.PAST, obj.PRESENT, obj.FUTURE];
+    const indexOfMaxValue = a.reduce(
+      (iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0
+    );
+    let orientation = `Unknown`;
     if (indexOfMaxValue === 0) {
-      orientation = 'Past'
+      orientation = 'Past';
     } else if (indexOfMaxValue === 1) {
-      orientation = 'Present'
+      orientation = 'Present';
     } else if (indexOfMaxValue === 2) {
-      orientation = 'Future'
+      orientation = 'Future';
     }
-    return orientation
-  }
+    return orientation;
+  };
 
   /**
-  * Analyse the temporal orientation of a string
-  * @function prospectimo
-  * @param {string} str  input string
-  * @param {Object} opts options
-  * @return {(Object||string)} temporal orientation or lexical value based on opts
-  */
+   * Analyse the temporal orientation of a string
+   * @function prospectimo
+   * @param {string} str    input string
+   * @param {Object} opts   options
+   * @return {(Object|string)}
+   */
   const prospectimo = (str, opts) => {
-    // error prevention
-    if (str == null) return null
-    if (typeof str !== 'string') str = str.toString()
-    // default options
-    if (opts == null) {
+    // no string return null
+    if (!str) {
+      console.error('prospectimo: no input found! Returning null.');
+      return null;
+    }
+    // if str isn't a string, make it into one
+    if (typeof str !== 'string') str = str.toString();
+    // trim whitespace and convert to lowercase
+    str = str.toLowerCase().trim();
+    // options defaults
+    if (!opts || typeof opts !== 'object') {
       opts = {
-        'return': 'lex',
         'encoding': 'binary',
-        'threshold': -999
-      }
+        'max': Number.POSITIVE_INFINITY,
+        'min': Number.NEGATIVE_INFINITY,
+        'nGrams': true,
+        'output': 'orientation',
+        'places': 9,
+        'sortBy': 'freq',
+        'wcGrams': false,
+      };
     }
-    opts.return = opts.return || 'lex'
-    opts.encoding = opts.encoding || 'binary'
-    opts.threshold = opts.threshold || -999
-    // convert to lowercase and trim whitespace
-    str = str.toLowerCase().trim()
+    opts.encoding = opts.encoding || 'binary';
+    opts.max = opts.max || Number.POSITIVE_INFINITY;
+    opts.min = opts.min || Number.NEGATIVE_INFINITY;
+    opts.nGrams = opts.nGrams || true;
+    opts.output = opts.output || 'orientation';
+    opts.places = opts.places || 9;
+    opts.sortBy = opts.sortBy || 'freq';
+    opts.wcGrams = opts.wcGrams || false;
+    const encoding = opts.encoding;
+    const output = opts.output;
+    const places = opts.places;
+    const sortBy = opts.sortBy;
     // convert our string to tokens
-    let tokens = tokenizer(str)
-    // if no tokens return null
-    if (tokens == null) return null
-    // get wordcount before we add n-grams
-    const wordcount = tokens.length
-    // get n-grams
-    const ngrams = []
-    ngrams.push(arr2string(simplengrams(str, 2)))
-    ngrams.push(arr2string(simplengrams(str, 3)))
-    const nLen = ngrams.length
-    let i = 0
-    for (i; i < nLen; i++) {
-      tokens = tokens.concat(ngrams[i])
+    let tokens = tokenizer(str);
+    // if there are no tokens return null
+    if (!tokens) {
+      console.warn('prospectimo: no tokens found. Returned null.');
+      return null;
     }
+    // get wordcount before we add ngrams
+    let wordcount = tokens.length;
+    // get n-grams
+    if (opts.nGrams) {
+      const bigrams = arr2string(simplengrams(str, 2));
+      const trigrams = arr2string(simplengrams(str, 3));
+      tokens = tokens.concat(bigrams, trigrams);
+    }
+    // recalculate wordcount if wcGrams is true
+    if (opts.wcGrams) wordcount = tokens.length;
     // get matches from array
-    const matches = getMatches(tokens, opts.threshold)
-    // calculate lexical useage
-    const enc = opts.encoding
-    const lex = {}
-    lex.PAST = calcLex(matches.PAST, wordcount, (-0.649406376419), enc)
-    lex.PRESENT = calcLex(matches.PRESENT, wordcount, 0.236749577324, enc)
-    lex.FUTURE = calcLex(matches.FUTURE, wordcount, (-0.570547567181), enc)
-    // predict and return
-    if (opts.return === 'lex') return lex
-    return getOrientation(lex)
-  }
+    const matches = getMatches(tokens, lexicon, opts.min, opts.max);
+    if (output === 'matches') {
+      return doMatches(matches, sortBy, wordcount, places, encoding);
+    } else if (output === 'full') {
+      const full = {
+        matches: doMatches(matches, sortBy, wordcount, places, encoding),
+        values: doValues(matches, places, encoding, wordcount),
+      };
+      return full;
+    } else if (output === 'lex') {
+      return doValues(matches, places, encoding, wordcount);
+    } else {
+      if (output !== 'orientation') {
+        console.warn('prospectimo: output option ("' + output +
+            '") is invalid, defaulting to "orientation".');
+      }
+      return getOrientation(doValues(matches, places, encoding, wordcount));
+    }
+  };
 
-  prospectimo.noConflict = function () {
-    root.prospectimo = previous
-    return prospectimo
-  }
+  prospectimo.noConflict = function() {
+    global.prospectimo = previous;
+    return prospectimo;
+  };
 
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = prospectimo
+      exports = module.exports = prospectimo;
     }
-    exports.prospectimo = prospectimo
+    exports.prospectimo = prospectimo;
   } else {
-    root.prospectimo = prospectimo
+    global.prospectimo = prospectimo;
   }
-}).call(this)
+}).call(this);
